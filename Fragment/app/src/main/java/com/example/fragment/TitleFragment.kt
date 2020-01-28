c98ca242401a16019b017d59c5e0756590a59c19
victor <victorkosgei254@gmail.com>
2020-01-28 08:28:48 +0300

    This requires more work, hard to work with someone else code

---
+++ Fragment/app/src/main/java/com/example/fragment/TitleFragment.kt
@@ -0,0 +1,24 @@
+package com.example.fragment
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+    }
+
+
+}
