c55b9b0d8eaa575ab8dd46c507e9d72ec0079a48
victor <victorkosgei254@gmail.com>
2020-01-30 10:55:28 +0300

    Created a menu added it to view

---
+++ NavigationDrawer/app/src/main/java/com/example/navigationdrawer/FirstFragement.kt
@@ -0,0 +1,27 @@
+package com.example.navigationdrawer
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FirstFragement : Fragment() {
+
+    companion object{
+        fun newInstance() = FirstFragement()
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first_fragement, container, false)
+    }
+
+
+}
--- NavigationDrawer/app/src/main/java/com/example/navigationdrawer/MainActivity.kt
+++ NavigationDrawer/app/src/main/java/com/example/navigationdrawer/MainActivity.kt
@@ -2,11 +2,39 @@ package com.example.navigationdrawer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.first_fragment -> {
+                val fragement = FirstFragement.newInstance()
+                replaceFragment(fragement)
+                true
+            }
+            R.id.second_fragment ->{
+                val fragment = SecondFragment.newInstance()
+                replaceFragment(fragment)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.my_menu,menu)
+        return true
+    }
+    private fun replaceFragment(fragement: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.FragementContainer,fragement)
+        fragmentTransaction.commit()
     }
 }
