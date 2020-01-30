442b060bdb07475f62c403e65e9b7ed0833720fb
victor <victorkosgei254@gmail.com>
2020-01-30 10:34:56 +0300

    Learning how to add Navigation Drawer

---
+++ Fragment/app/src/main/java/com/example/fragment/Fragment3.kt
@@ -0,0 +1,22 @@
+package com.example.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Fragment3 :Fragment(){
+    companion object{
+        fun newInstance() = Fragment3()
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.third_fragment,container,false)
+    }
+}
--- Fragment/app/src/main/java/com/example/fragment/MainActivity.kt
+++ Fragment/app/src/main/java/com/example/fragment/MainActivity.kt
@@ -2,15 +2,50 @@ package com.example.fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.ClassCastException
 
 class MainActivity : AppCompatActivity() {
 
+//    val drawerToggle by lazy {
+//        ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close)
+//    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    navigationView.setNavigationItemSelectedListener {
+        selectDrawerIter(it)
+        true
+    }
+    }
+
+    private fun selectDrawerIter(item:MenuItem){
+        var fragment : Fragment ?= null
+        val fragmentClass = when(item.itemId){
+            R.id.first_fragment -> FragmentSample::class.java
+            R.id.second_fragment -> Fragment2::class.java
+            R.id.third_fragment -> Fragment3::class.java
+            else -> FragmentSample::class.java //this is a class reference, means to access that class object
+
+        }
+        try {
+
+            fragment=fragmentClass.newInstance() as Fragment
+        } catch (e:ClassCastException){
+
+            e.printStackTrace()
+        }
+
+        replaceFragment(fragment)
+        drawerLayout.closeDrawer(GravityCompat.START)
 
     }
 
@@ -26,6 +61,11 @@ class MainActivity : AppCompatActivity() {
                 replaceFragment(fragment)
                 true
             }
+            R.id.third_fragment->{
+                val fragment = Fragment3.newInstance()
+                replaceFragment(fragment)
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
@@ -33,10 +73,10 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.fragment_menu,menu)
         return true
     }
-    private fun replaceFragment(fragment: Fragment)
+    private fun replaceFragment(fragment: Fragment?)
     {
         val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.FragmentContainer,fragment)
+        fragmentTransaction.replace(R.id.FragmentContainer, fragment!!)
         fragmentTransaction.commit()
     }
 }
