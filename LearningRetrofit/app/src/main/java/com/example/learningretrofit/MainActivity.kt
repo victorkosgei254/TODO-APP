869b3a98703b161882d1fbc9e3e4ce7acb7e4c98
victor <victorkosgei254@gmail.com>
2020-02-03 08:41:30 +0300

    Working on retrofit

--- LearningRetrofit/app/src/main/java/com/example/learningretrofit/MainActivity.kt
+++ LearningRetrofit/app/src/main/java/com/example/learningretrofit/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.learningretrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getJSON()
+    }
+
+    fun getJSON()
+    {
+        val url = "https://api.letsbuildthatapp.com/youtube/home_feed"
+        val request = Request.Builder().url(url).build()
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object :Callback {
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+                println(body)
+
+                val gson = GsonBuilder().create()
+
+
+            }
+            override fun onFailure(call: Call, e: IOException) {
+                    println("Failed to fetch request")
+            }
+        })
     }
 }
